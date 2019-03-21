d7a7aa98c867071d81dde0b9195e352a84849667
ric0303 <robert.curry@daugherty.com>
2019-03-20 20:19:19 -0400

    changed script run directory

--- run-regression.sh
+++ run-regression.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/kinesis-cli/kinesis --path /Users/ric0303/Documents/DBmetrics/test/DBmetrics-regression-test/kinesis.json --canvas-timeout=120 --output /Users/ric0303/Documents/DBmetrics/test/DBmetrics-regression-test/reports --context-vars /Users/ric0303/Documents/DBmetrics/context/DBmetrics.json --max-data-rows 250 --driver-name chromedriver --accept-language en-US
+~/bin/kinesis-cli/kinesis --path /Users/ric0303/Documents/DBmetrics/test/DBmetrics-regression-test/kinesis.json --canvas-timeout=120 --output /Users/ric0303/Documents/DBmetrics/test/DBmetrics-regression-test/reports --context-vars /Users/ric0303/Documents/DBmetrics/context/DBmetrics.json --max-data-rows 250 --driver-name chromedriver --accept-language en-US
